06da2084239f651a502242762869cd6ee3a41586
luis-tapiaa <luis.tapiaa@hotmail.com>
2021-02-25 15:05:56 -0600

    Add controllers

---
+++ controllers/areaPreocupacionController.js
@@ -0,0 +1,36 @@
+const Model = require('../models/Areas_preocupacion');
+
+exports.crear = async(req, res) => {
+    try {
+        const item = await Model.create(req.body);
+        return res.status(200).json({ item });
+    } catch (error) {
+        return res.status(404).send(error);
+    }
+};
+
+exports.eliminar = async(req, res) => {
+    const { id } = req.params;
+    const response = await Model.destroy({ where: { id } });
+
+    if(response) {
+        res.status(200).json({ message: 'Eliminado' });
+    }
+};
+
+exports.getAll = async(req, res) => {
+    const items = await Model.findAll();
+    res.status(200).json({ items });
+};
+
+exports.editar = async(req, res) => {
+    const { id } = req.params;
+   
+    const item = await Model.findOne({ where: { id } });
+
+    Object.keys(req.body).forEach(key => {
+        item[key] = req.body[key];
+    });
+    await item.save();
+    res.status(200).json({ item });
+};
